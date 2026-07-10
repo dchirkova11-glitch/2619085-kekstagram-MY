6956c5c728efab5dbe50e0c5dad67168ae5817a6
Дарья Чиркова <d.chirkova11@gmail.com>
2026-07-10 10:18:47 +0300

    ДЗ 5.16. Функции возвращаются

--- js/functions.js
+++ js/functions.js
@@ -36,3 +36,24 @@ const extractDigits = (string) => {
 };
 
 extractDigits('Цена: 1500 руб.');
+
+//5.16. Функции возвращаются
+
+function isMeetingInWorkingHours(workStart, workEnd, meetingStart, duration) {
+
+  const parseTimeToMinutes = (timeStr) => {
+    const [hours, minutes] = timeStr.split(':').map(Number);
+    return hours * 60 + minutes;
+  };
+
+  const startWorkMin = parseTimeToMinutes(workStart);
+  const endWorkMin = parseTimeToMinutes(workEnd);
+  const startMeetingMin = parseTimeToMinutes(meetingStart);
+
+  const endMeetingMin = startMeetingMin + duration;
+
+  return startMeetingMin >= startWorkMin && endMeetingMin <= endWorkMin;
+}
+// console.log(isMeetingInWorkingHours('08:00', '17:00', '08:30', 30));
+// console.log(isMeetingInWorkingHours('8:5', '16:00', '15:30', 45));
+// console.log(isMeetingInWorkingHours('09:00', '18:00', '09:00', 540));
