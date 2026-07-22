bf0a92a98c8da34bfd4357765f39de9788b6432e
Дарья Чиркова <d.chirkova11@gmail.com>
2026-07-22 15:25:40 +0300

    ДЗ 8.14. Открывается и закрывается

--- js/render-photos.js
+++ js/render-photos.js
@@ -1,3 +1,5 @@
+import { openModal } from './big-picture.js';
+
 const similarPhotosElement = document.querySelector('.pictures');
 const similarPhotosTemplate = document.querySelector('#picture').content.querySelector('.picture');
 
@@ -5,12 +7,18 @@ export const renderPhotos = (photos) => {
 
   const similarListFragment = document.createDocumentFragment();
 
-  photos.forEach((photo) => {
-    const photosElement = similarPhotosTemplate.cloneNode(true);
-    photosElement.querySelector('.picture__img').src = photo.url;
-    photosElement.querySelector('.picture__comments').textContent = photo.comments.length;
-    photosElement.querySelector('.picture__likes').textContent = photo.likes;
-    similarListFragment.appendChild(photosElement);
+  photos.forEach(({ url, description, comments, likes }) => {
+    const newPhotoElement = similarPhotosTemplate.cloneNode(true);
+    const imageElement = newPhotoElement.querySelector('.picture__img');
+    imageElement.src = url;
+    imageElement.alt = description;
+    newPhotoElement.querySelector('.picture__comments').textContent = comments.length;
+    newPhotoElement.querySelector('.picture__likes').textContent = likes;
+    similarListFragment.appendChild(newPhotoElement);
+
+    newPhotoElement.addEventListener('click', () => {
+      openModal({ url, description, comments, likes });
+    });
   });
   similarPhotosElement.appendChild(similarListFragment);
 };
